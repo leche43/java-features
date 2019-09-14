4817b3f73f0448fbb4532baf31bc8d4b0b0551fe
Thomas Gonzalez <thomas.gonzalez@capgemini.com>
2019-09-14 11:06:23 +0200

    Maven init with archetype:generate

---
+++ src/test/java/io/leche43/selflearning/AppTest.java
@@ -0,0 +1,20 @@
+package io.leche43.selflearning;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
